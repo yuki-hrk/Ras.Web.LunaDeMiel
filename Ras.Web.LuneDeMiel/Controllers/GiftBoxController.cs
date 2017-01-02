98b6929f29ef402387d1f78b52b638dc83e5cfbf
yuki-hrk <sanchespeier_536@yahoo.co.jp>
2017-01-02 18:34:21 +0900

    Revert "Revert "機能のガワだけ追加""
    This reverts commit ee65f1dbcdbedb3e05caa2c62f5e9cd65be428c2.

---
+++ Ras.Web.LuneDeMiel/Controllers/GiftBoxController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ras.Web.LuneDeMiel.Controllers
+{
+    /// <summary>
+    /// ギフトボックス機能 のコントローラ
+    /// </summary>
+    public class GiftBoxController : Controller
+    {
+        // GET: GiftBox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
