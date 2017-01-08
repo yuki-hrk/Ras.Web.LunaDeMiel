a84e18ea7dc99dce8ad1de4b625c250bdec84cba
yuki-hrk <sanchespeier_536@yahoo.co.jp>
2017-01-08 18:06:39 +0900

    gitignore テスト

--- Ras.Web.LuneDeMiel/ViewModels/HoneyViewModel.cs
+++ Ras.Web.LuneDeMiel/ViewModels/HoneyViewModel.cs
@@ -5,11 +5,11 @@ using System.Web;
 
 namespace Ras.Web.LuneDeMiel.ViewModels
 {
+    /// <summary>
+    /// はちみつ の画面表示項目を表します。
+    /// </summary>
     public class HoneyViewModel
     {
-        // こめんと
-        // テストコメント kuroishi
-
-        // テストコメント obonai2
+        // TODO
     }
 }
