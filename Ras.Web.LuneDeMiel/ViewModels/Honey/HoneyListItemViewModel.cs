521e814c45f7ee3703162e6a65518ce1a52a29d3
fujita-jouichirou <jouichi.923@gmail.com>
2017-08-11 20:55:05 +0900

    コミット漏れ<(__)>

---
+++ Ras.Web.LuneDeMiel/ViewModels/Honey/HoneyListItemViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ras.Web.LuneDeMiel.ViewModels.Honey
+{
+    /// <summary>
+    /// はちみつ一覧レコードモデル
+    /// </summary>
+    public class HoneyListItemViewModel
+    {
+        /// <summary> ID </summary>
+		public int Id { get; set; }
+		/// <summary> 商品名 </summary>
+		public string Name { get; set; }
+		/// <summary> サイズ・値段ディクショナリ </summary>
+		public Dictionary<int, int> SizePriceDictionary { get; set; }
+		/// <summary> 地域名 </summary>
+        public string Municipal { get; set; }
+		/// <summary> 花の名前 </summary>
+		public string FlowerName { get; set; }
+        /// <summary> 画像データ </summary>
+        public byte[] imageBytes { get; set; }
+	}
+}
