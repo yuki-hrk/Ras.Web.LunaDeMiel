5be2f77f5abde6ecd710f197fd19c4be4a8fc83d
yuki-hrk <sanchespeier_536@yahoo.co.jp>
2018-02-03 17:27:42 +0900

    食べ合わせの一覧・詳細モック作成

--- Ras.Web.LuneDeMiel/Controllers/MariageController.cs
+++ Ras.Web.LuneDeMiel/Controllers/MariageController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Ras.Web.LuneDeMiel.Helpers;
+using Ras.Web.LuneDeMiel.ViewModels.Mariage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,10 +13,73 @@ namespace Ras.Web.LuneDeMiel.Controllers
     /// </summary>
     public class MariageController : Controller
     {
-        // GET: Mariage
-        public ActionResult Index()
+        // GET: /Mariage
+        // GET: /Mariage/Index?page={page}
+        public ActionResult Index(int? page)
         {
-            return View();
+            var countries = new SelectList(
+                    new Dictionary<int, string>
+                    {
+                        { 0, string.Empty },
+                        { 1, "日本" },
+                        { 2, "中国" },
+                        { 3, "ギリシャ" }
+                    },
+                    "Key",
+                    "Value"
+                );
+
+            var categories = new SelectList(
+                    new Dictionary<int, string>
+                    {
+                        { 0, string.Empty },
+                        { 1, "フルーツ" },
+                        { 2, "草花" },
+                        { 3, "樹木" }
+                    },
+                    "Key",
+                    "Value"
+                );
+
+            var mariages = Enumerable.Range(1, 500)
+                                     .Select(x => new MariageRowViewModel
+                                     {
+                                         Id = x,
+                                         ProductName = "JA青森りんご",
+                                         HaneyCategory = "フルーツ",
+                                         Area = "日本 青森県",
+                                         FoodName = "ロシアンティー",
+                                         Content = "説明...",
+                                         Comment = "感想..."
+                                     });
+
+            var vm = new MariageListViewModel
+            {
+                Countries = countries,
+                HoneyCategories = categories,
+                SearchCond = new MariageSearchCondViewModel(),
+                Rows = PagedListHelper.NewInstance(mariages, page ?? 1)
+            };
+
+            return View(vm);
+        }
+
+        // GET: /Mariage/Detail?id={id}
+        public ActionResult Detail(int id)
+        {
+            var vm = new MariageDetailViewModel
+            {
+                Id = id,
+                ProductName = "JA青森りんご",
+                HoneyCategory = "フルーツ",
+                Country = "日本",
+                MunicipalName = "青森県",
+                FoodName = "ロシアンティー",
+                Content = "説明<br />説明<br />説明",
+                Comment = "感想<br />感想<br />感想"
+            };
+
+            return View(vm);
         }
     }
 }
---
+++ Ras.Web.LuneDeMiel/ViewModels/Mariage/MariageRowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ras.Web.LuneDeMiel.ViewModels.Mariage
+{
+    /// <summary>
+    /// 食べ合わせ一覧における一行のデータを表します。
+    /// </summary>
+    public class MariageRowViewModel
+    {
+        /// <summary> ID </summary>
+		public int Id { get; set; }
+
+        /// <summary> 画像(BLOB文字列) </summary>
+        public string Image { get; set; }
+
+        /// <summary> 商品名 </summary>
+        public string ProductName { get; set; }
+
+        /// <summary> 分類 </summary>
+        public string HaneyCategory { get; set; }
+
+        /// <summary> 地域 </summary>
+        public string Area { get; set; }
+
+        /// <summary> 食品名 </summary>
+        public string FoodName { get; set; }
+
+        /// <summary> 説明 </summary>
+        public string Content { get; set; }
+
+        /// <summary> コメント </summary>
+        public string Comment { get; set; }
+    }
+}
